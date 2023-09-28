7b451aadc90a771e99a57e4ed61d3034bb2380c7
lifetecti <139556710+lifetecti@users.noreply.github.com>
2023-09-28 14:56:46 -0300

    Create permissions.sh

---
+++ permissions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Fazendo backup do arquivo de sudoers..."
+
+cp /etc/sudoers /etc/sudoers.bkp
+
+echo "Editando o arquivo de sudoers..."
+
+cp /etc/sudoers /tmp/sudoers.tmp
+
+cat << EOF >> /tmp/sudoers.tmp
+
+#Alias de comandos
+Cmnd_Alias NOROOT = !/usr/bin/su, !/bin/bash
+Cmnd_Alias VIEW_IPTABLES= /usr/sbin/iptables -L, /usr/sbin/iptables -nL
+
+#Grupos definidos no AD
+%life_administradores@ad-auth.life.com.br	ALL=(ALL) ALL, NOROOT
+%life_operador@ad-auth.life.com.br		    ALL=(ALL) ALL, NOROOT, !/sbin/shutdown, !/sbin/reboot, !/usr/bin/rm
+%life_viewer@ad-auth.life.com.br		    ALL=(ALL) /usr/bin/cat, VIEW_IPTABLES
+
+EOF
+
+mv -f /tmp/sudoers.tmp /etc/sudoers
+
+echo "Finalizado configuração"
+
+
+
